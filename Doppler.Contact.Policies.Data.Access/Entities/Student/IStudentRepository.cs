f87d1a948a0765515d4da167ab35ce03cff4c6d2
jkortega9008 <jkortega90@gmail.com>
2021-06-03 18:29:10 -0300

    chore:  add generic respository

---
+++ Doppler.Contact.Policies.Data.Access/Entities/Student/IStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Doppler.Contact.Policies.Data.Access.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doppler.Contact.Policies.Data.Access.Entities.Student
+{
+   public interface IStudentRepository : IGenericRepository<Student>
+    {
+
+    }
+}
---
+++ Doppler.Contact.Policies.Data.Access/Entities/Student/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doppler.Contact.Policies.Data.Access.Entities.Student
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+    }
+}

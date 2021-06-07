defb06ef98f5643688d03f9c17e27b1f4b0a78c6
gsi-ortega <ortega@generalsoftwareinc.com>
2021-06-07 15:04:10 -0400

    chore: add doppler authorization requirements for superUser and Owner

---
+++ Doppler.Contact.Policies.Api/DopplerSecurity/DopplerAuthorizationRequirement.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace doppler_contact_policies_api.DopplerSecurity
+{
+    public class DopplerAuthorizationRequirement : IAuthorizationRequirement
+    {
+        public bool AllowSuperUser { get; init; }
+        public bool AllowOwnResource { get; init; }
+    }
+}

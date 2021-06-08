919c5a2aa6a406d5a7343dc89fe89598956b55a4
gsi-ortega <ortega@generalsoftwareinc.com>
2021-06-08 00:34:06 -0400

    fix: run prittier and eclint verify commands. Add .user as ignore file in prettier

--- Doppler.Contact.Policies.Api/DopplerSecurity/DopplerSecurityServiceCollectionExtensions.cs
+++ Doppler.Contact.Policies.Api/DopplerSecurity/DopplerSecurityServiceCollectionExtensions.cs
@@ -20,31 +20,31 @@ namespace doppler_contact_policies_api.DopplerSecurity
             services.ConfigureOptions<ConfigureDopplerSecurityOptions>();
 
             services.AddOptions<AuthorizationOptions>()
-                  .Configure(o =>
-                  {
-                      var simpleAuthenticationPolicy = new AuthorizationPolicyBuilder()
-                          .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-                          .RequireAuthenticatedUser()
-                          .Build();
+                .Configure(o =>
+                {
+                    var simpleAuthenticationPolicy = new AuthorizationPolicyBuilder()
+                        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                        .RequireAuthenticatedUser()
+                        .Build();
 
-                      var onlySuperUserPolicy = new AuthorizationPolicyBuilder()
-                          .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-                          .AddRequirements(new DopplerAuthorizationRequirement()
-                          {
-                              AllowSuperUser = true
-                          })
-                          .RequireAuthenticatedUser()
-                          .Build();
+                    var onlySuperUserPolicy = new AuthorizationPolicyBuilder()
+                        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                        .AddRequirements(new DopplerAuthorizationRequirement()
+                        {
+                            AllowSuperUser = true
+                        })
+                        .RequireAuthenticatedUser()
+                        .Build();
 
-                      var ownResourceOrSuperUserPolicy = new AuthorizationPolicyBuilder()
-                          .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-                          .AddRequirements(new DopplerAuthorizationRequirement()
-                          {
-                              AllowSuperUser = true,
-                              AllowOwnResource = true
-                          })
-                          .RequireAuthenticatedUser()
-                          .Build();
+                    var ownResourceOrSuperUserPolicy = new AuthorizationPolicyBuilder()
+                        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                        .AddRequirements(new DopplerAuthorizationRequirement()
+                        {
+                            AllowSuperUser = true,
+                            AllowOwnResource = true
+                        })
+                        .RequireAuthenticatedUser()
+                        .Build();
 
                     // TODO: I would like to use ownResourceOrSuperUserPolicy as the default policy, but I
                     // cannot override a more restrictive policy with a less restrictive one. So,
@@ -52,9 +52,9 @@ namespace doppler_contact_policies_api.DopplerSecurity
                     // controller.
                     o.DefaultPolicy = simpleAuthenticationPolicy;
 
-                      o.AddPolicy(Policies.ONLY_SUPERUSER, onlySuperUserPolicy);
-                      o.AddPolicy(Policies.OWN_RESOURCE_OR_SUPERUSER, ownResourceOrSuperUserPolicy);
-                  });
+                    o.AddPolicy(Policies.ONLY_SUPERUSER, onlySuperUserPolicy);
+                    o.AddPolicy(Policies.OWN_RESOURCE_OR_SUPERUSER, ownResourceOrSuperUserPolicy);
+                });
 
             services.AddOptions<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme)
                 .Configure<IOptions<DopplerSecurityOptions>>((o, securityOptions) =>

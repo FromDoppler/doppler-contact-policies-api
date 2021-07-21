0165561c183bf25903eb5494d4a03694fdf051f0
gsi-ortega <ortega@generalsoftwareinc.com>
2021-07-21 15:42:26 -0400

    refactor: improve log error message and use IOptions

--- Doppler.ContactPolicies.Business.Logic/UserApiClient/Services/UserFeaturesService.cs
+++ Doppler.ContactPolicies.Business.Logic/UserApiClient/Services/UserFeaturesService.cs
@@ -1,6 +1,7 @@
 using Doppler.ContactPolicies.Business.Logic.DTO;
-using Microsoft.Extensions.Configuration;
+using Doppler.ContactPolicies.Business.Logic.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
@@ -11,20 +12,20 @@ namespace Doppler.ContactPolicies.Business.Logic.UserApiClient.Services
     public class UserFeaturesService : IUserFeaturesService
     {
         private readonly IHttpClientFactory _clientFactory;
-        private readonly IConfiguration _configuration;
+        private readonly IOptions<UserFeaturesServiceSettings> _userFeaturesServiceSettings;
         private readonly ILogger<UserFeaturesService> _logger;
 
-        public UserFeaturesService(IHttpClientFactory clientFactory, IConfiguration configuration, ILogger<UserFeaturesService> logger)
+        public UserFeaturesService(IHttpClientFactory clientFactory, IOptions<UserFeaturesServiceSettings> userFeaturesServiceSettings, ILogger<UserFeaturesService> logger)
         {
             _clientFactory = clientFactory;
-            _configuration = configuration;
+            _userFeaturesServiceSettings = userFeaturesServiceSettings;
             _logger = logger;
         }
         public async Task<bool> GetUserContactPoliciesFeatureAsync(string accountName)
         {
             try
             {
-                var baseUri = _configuration.GetSection("UsersApiURL").Value;
+                var baseUri = _userFeaturesServiceSettings.Value.UsersApiURL;
                 var uri = new Uri(baseUri + $"/accounts/{accountName}/features");
 
                 var client = _clientFactory.CreateClient("users-api");
@@ -33,9 +34,9 @@ namespace Doppler.ContactPolicies.Business.Logic.UserApiClient.Services
                 var features = JsonConvert.DeserializeObject<Features>(responseString);
                 return features.ContactPolicies;
             }
-            catch (HttpRequestException httpRequestException)
+            catch (Exception exception)
             {
-                _logger.LogError(httpRequestException.Message, "An error occurred.");
+                _logger.LogError(exception, $"Failed to get contact policies feature with account name {accountName}");
                 throw;
             }
         }
